af88f5a7774cf3074ac2b035eb57610829a405c5
insert314Here <89361627+insert314Here@users.noreply.github.com>
2021-11-03 16:25:22 -0400

    started work on making a fibonacci sequence counter

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+
+namespace Fibonacci
+{//WORK IN PROGRESS
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! This application will write out the numbers in the fibonacci sequence and sum there values");
+            Console.WriteLine("How many numbers would you like to be displayed, must be greater than 3");
+            //have user select the length of the sequence; must be greater than 3
+
+        }
+
+        //start with the number 0, so first two are 1
+        static void CountFib(int fibLength)
+        {
+            int[] fibList = new int[fibLength] { };
+            fibLength.
+            int prevNum;
+            int nextNum;
+            nextNum = initial + prevNum;
+        }
+        //define how to get the next number: 1, 1, 2, 3-->
+
+
+
+        //display the selected sequence
+
+        //sum the values of the given sequence length
+    }
+
+}
