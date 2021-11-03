1b58df97864041b0908ed4f32da875ef19abc399
insert314Here <89361627+insert314Here@users.noreply.github.com>
2021-11-03 14:42:05 -0400

    basic use of stream reader

---
+++ FileIO/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+class ReadFile
+{
+    public static void Main()
+    {
+        try
+        {
+            string path = @"c:\temp2\temp.txt";
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+                Console.ReadLine();
+            }
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("File not found");
+        }
+
+    }
+}
---
+++ IntroToClasses/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace IntroToClasses
+{
+    public class Constructors
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("write three names and press enter key");
+            int total = 3;
+            Person[] persons = new Person[total];
+
+            for (int i = 0; i < total; i++)
+            {
+                persons[i] = new Person(Console.ReadLine());
+            }
+            for (int i = 0; i < total; i++)
+            {
+                Console.WriteLine(persons[i].ToString());
+            }
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public Person(string name) //constructor
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"Hello my name is {Name}...slim shady";
+        }
+
+        ~Person() //indicates a destructor
+        {
+            Name = string.Empty;
+        }
+    }
+}
