a5f0eb1891da09c909a5d68e0f09bc83b2f9fe0d
insert314Here <89361627+insert314Here@users.noreply.github.com>
2021-11-12 19:10:25 -0500

    completed fib sequence but did not add sum function yet

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace Fibonacci
 {//WORK IN PROGRESS
@@ -7,22 +8,53 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! This application will write out the numbers in the fibonacci sequence and sum there values");
-            Console.WriteLine("How many numbers would you like to be displayed, must be greater than 3");
-            //have user select the length of the sequence; must be greater than 3
+            int nthTerm;
 
+            Console.WriteLine("Hello World! This application will write out the numbers in the fibonacci sequence and sum there values");
+            Console.WriteLine("How many numbers would you like to be displayed, or the nth term for you maths folk");
+            //have user select the length of the sequence; nTH term
+            nthTerm = Convert.ToInt32(Console.ReadLine());
+            CountFib(nthTerm);
         }
 
-        //start with the number 0, so first two are 1
+        //                      I, N, P+N-->P , 
+        //start with the number 0, 1,  1      ,  2
+        //                         P,  N      , P+N
+        //index 4 uses sum property
+
         static void CountFib(int fibLength)
         {
-            int[] fibList = new int[fibLength] { };
-            fibLength.
-            int prevNum;
-            int nextNum;
-            nextNum = initial + prevNum;
+            int next = 0;
+            int prev = 0;
+            List<int> fibList = new List<int>();
+            fibList.Add(next); //0 is first added to the list
+
+            for (int i = 0; i < fibLength; i++)
+            {
+                if (next == 0)
+                {
+                    next = 1;
+                    fibList.Add(next);//1 is add to the list
+                }
+                else
+                {
+                    //   1   =   1  +   0   --> results after 1st iteration of loop
+                    //           =  1
+                    int tempNext = next;
+                    // 1 =  1   +  0
+                    next = next + prev;
+                    fibList.Add(next); //1 is added to the list afain after 1st iteration
+                    prev = tempNext;
+                }
+            }
+
+
+            foreach (int item in fibList)
+            {
+                Console.Write($"{item}, ");
+            }
         }
-        //define how to get the next number: 1, 1, 2, 3-->
+
 
 
 
